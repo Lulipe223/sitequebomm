201cb4d472080ccfb7124f6c06e228c69a2a565b
Lulipe223 <almeida.luizfelipe20@gmail.com>
2025-04-29 23:35:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para mostrar ou ocultar os detalhes de cada prato
+function toggleDetails(detailId) {
+    const detailElement = document.getElementById(detailId);
+    detailElement.classList.toggle('show');
+}
